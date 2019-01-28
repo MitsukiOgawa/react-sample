d81b92b427f6f9bf1fbb5eda1b92f354c3915280
Mitsuki Ogawa <mitsu.oga.0210@gmail.com>
2019-01-28 20:23:46 +0900

    learn actions and type

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// アクションクリエーター
+export const increment = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+
+export const decrement = () => {
+    return {
+        type: DECREMENT
+    }
+}
